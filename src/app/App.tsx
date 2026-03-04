011d81055b4612991e4d1e36eca984fa0a69be4e
Raturchik <artyrkafarov@gmail.com>
2026-03-04 21:36:00 +0300

    fix: set correct root path for RootLayout and remove unused routes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,13 +8,7 @@ import {
 
 export function App() {
   const routes = createRoutesFromElements(
-    <Route element={<RootLayout />}>
-      <Route path="/" /*element={ Главная страница } */ />
-      <Route path="/" /*element={ Уроки } */ />
-      <Route path="/" /*element={ Оценки } */ />
-      <Route path="/" /*element={ Четверти } */ />
-      <Route path="/" /*element={ Аналитика } */ />
-    </Route>,
+    <Route path="/" element={<RootLayout />}></Route>,
   );
   const router = createBrowserRouter(routes);
   return <RouterProvider router={router} />;
