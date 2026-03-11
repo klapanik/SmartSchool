ab99dcd003f4b5c02b2dbf1f5ff474a87210a936
Lizonka-spec <proudoffyou01@gmail.com>
2026-03-11 20:39:18 +0300

    fix: correct tailwind classes

--- src/widgets/AppSidebar.tsx
+++ src/widgets/AppSidebar.tsx
@@ -5,7 +5,6 @@ import {
   SidebarGroupLabel,
   SidebarHeader,
   SidebarTrigger,
-  useSidebar,
 } from "@/components/ui/sidebar";
 
 import {
@@ -18,17 +17,16 @@ import { Kbd, KbdGroup } from "@/components/ui/kbd";
 import { GraduationCap } from "lucide-react";
 
 export function AppSidebar() {
-  const { open } = useSidebar();
-
   return (
-    <div>
+    <div className="relative h-full">
       <Sidebar>
-        <div className="flex p-5 flex-col gap-12.5 h-full relative">
-          <SidebarHeader className="flex flex-row gap-3.25">
-            <GraduationCap className="w-7.5 h-7.5 my-auto cursor-pointer" />
+        <div className="px-4 py-5 h-full">
+          <SidebarHeader className="flex flex-row mb-5">
+            <GraduationCap className="my-auto cursor-pointer mr-2" />
+
             <div>
               <h2 className="font-semibold text-2xl">Дневник</h2>
-              <p className="text-sm font-medium">Оценок</p>
+              <p className="text-sm">Оценок</p>
             </div>
           </SidebarHeader>
           <SidebarContent>
@@ -41,21 +39,15 @@ export function AppSidebar() {
         </div>
       </Sidebar>
 
-      <div
-        className="flex flex-col h-full transition-all duration-300 ease-in-out"
-        style={{
-          marginLeft: open ? "280px" : "0px",
-          width: open ? "calc(100vw - 280px)" : "100vw",
-        }}
-      >
+      <div className="h-full relative">
         <Tooltip>
           <TooltipTrigger asChild>
-            <SidebarTrigger className="cursor-pointer hover:bg-smoky-white" />
+            <SidebarTrigger
+              className="transition-colors duration-300 hover:bg-smoky-white
+                      absolute -right-9 top-4 cursor-pointer"
+            />
           </TooltipTrigger>
-          <TooltipContent
-            side="bottom"
-            className="flex items-center gap-2 p-2 shadow-[0px_2px_10px_0px_var(--primary)]"
-          >
+          <TooltipContent side="bottom" className="flex items-center gap-2 p-2">
             <span className="text-sm text-primary">Боковая панель</span>
             <KbdGroup>
               <Kbd>Ctrl</Kbd>
