1a1cd1bb812efa600b181add58983acb98374399
Konstantin <lapanik.kostik@gmail.com>
2026-03-15 21:01:05 +0300

    ESG-16: create AppHeader component

---
+++ src/widgets/AppHeader/AppHeader.tsx
@@ -0,0 +1,9 @@
+export function AppHeader() {
+    return (
+        <header className="h-14 flex border-b w-full">
+            <h1 className="ml-11 my-auto font-semibold text-xl text-primary-foreground sm:ml-12 sm:text-2xl">
+                Дневник Оценок
+            </h1>
+        </header>
+    );
+}
