fffb68ac58ea0ff585837c70c54838a921b5da10
Kostik Lapanik <lapanik.kostik@gmail.com>
2026-03-15 20:45:36 +0300

    feat: add media for AppHeader (with tailwind css)

--- src/widgets/AppHeader/AppHeader.tsx
+++ src/widgets/AppHeader/AppHeader.tsx
@@ -1,7 +1,7 @@
 export function AppHeader() {
     return (
         <header className="h-14 flex border-b w-full">
-            <h1 className="ml-14 my-auto font-semibold text-2xl text-primary-foreground">
+            <h1 className="ml-11 my-auto font-semibold text-xl text-primary-foreground sm:ml-12 sm:text-2xl">
                 Дневник Оценок
             </h1>
         </header>
