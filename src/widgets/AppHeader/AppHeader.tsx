61a603f0155e174db23fe235c4d2f041587ec4da
Kostik Lapanik <lapanik.kostik@gmail.com>
2026-03-15 20:42:02 +0300

    feat: add AppHeader ui layout

--- src/widgets/AppHeader/AppHeader.tsx
+++ src/widgets/AppHeader/AppHeader.tsx
@@ -1,3 +1,9 @@
 export function AppHeader() {
-    return <div>AppHeader</div>;
+    return (
+        <header className="h-14 flex border-b w-full">
+            <h1 className="ml-14 my-auto font-semibold text-2xl text-primary-foreground">
+                Дневник Оценок
+            </h1>
+        </header>
+    );
 }
