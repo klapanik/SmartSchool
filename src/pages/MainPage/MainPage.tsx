70a946ff4b61097969191c517aa6c1f3f5e036a2
Kostik Lapanik <lapanik.kostik@gmail.com>
2026-03-16 21:23:10 +0300

    feat: add hero section layout (with styles) and integrate it into main page

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,3 +1,9 @@
+import { Hero } from "@/widgets/main-page/ui/Hero/Hero";
+
 export function MainPage() {
-    return <div>MainPage</div>;
+    return (
+        <section>
+            <Hero />
+        </section>
+    );
 }
