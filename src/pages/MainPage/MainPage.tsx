4aa0e3087e4461515ae962eb26c7c335bd082769
VladimirYastrebov <vladimir.i.yastrebov@gmail.com>
2026-03-21 13:30:57 +0300

    add empty state for today's schedule

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,9 +1,11 @@
 import { Hero } from "@/widgets/main-page/ui/Hero/Hero";
+import { EmptyTodaysSchedule } from "@/widgets/main-page/ui/TodaysSchedule/EmptyTodaysSchedule";
 
 export function MainPage() {
     return (
         <section>
             <Hero />
+            <EmptyTodaysSchedule />
         </section>
     );
 }
--- src/widgets/main-page/ui/TodaysSchedule/EmptyTodaysSchedule.tsx
+++ src/widgets/main-page/ui/TodaysSchedule/EmptyTodaysSchedule.tsx
@@ -1,3 +1,50 @@
+import {
+    Empty,
+    EmptyContent,
+    EmptyDescription,
+    EmptyHeader,
+    EmptyMedia,
+    EmptyTitle,
+} from "@/components/ui/empty";
+import { Calendar } from "lucide-react";
+import { Link } from "react-router-dom";
+
 export function EmptyTodaysSchedule() {
-    return <div>EmptyTodaysSchedule</div>;
+    return (
+        <div
+            style={{
+                color: "var(--smoky-black)",
+                backgroundColor: "var(--smoky-white)",
+                borderColor: "var(--smoky-white)",
+                borderRadius: "var(--radius-lg)",
+                padding: "var(--p-6)",
+                boxShadow: "var(--shadow-lg)",
+            }}
+        >
+            <Empty>
+                <EmptyHeader>
+                    <EmptyTitle>Расписание на сегодня</EmptyTitle>
+                    <EmptyDescription>суббота, 21 февраля</EmptyDescription>
+                </EmptyHeader>
+                <EmptyContent>
+                    <div className="mb-4 mx-auto">
+                        <EmptyMedia variant="icon">
+                            <Calendar />
+                        </EmptyMedia>
+                    </div>
+                    <h4 className="text-base mb-0.5">Сегодня уроков нет</h4>
+                    <p className="text-sm text-muted-foreground">Отличный день для отдыха!</p>
+                </EmptyContent>
+                <EmptyDescription>
+                    <Link
+                        style={{ textDecoration: "none" }}
+                        to="/schedule"
+                        className="bg-primary px-4 py-2 rounded-lg text-white transition-colors duration-300 hover:bg-white hover:text-primary"
+                    >
+                        Перейти в расписание
+                    </Link>
+                </EmptyDescription>
+            </Empty>
+        </div>
+    );
 }
