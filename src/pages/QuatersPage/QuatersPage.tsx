1fbfc8e0a023f57ceb5cd14e2a2644884006bed1
Kostik Lapanik <lapanik.kostik@gmail.com>
2026-03-24 17:25:54 +0300

    feat: add QuatersPage layout

--- src/pages/QuatersPage/QuatersPage.tsx
+++ src/pages/QuatersPage/QuatersPage.tsx
@@ -1,3 +1,10 @@
 export function QuatersPage() {
-    return <div>QuatersPage</div>;
+    return (
+        <section>
+            <div className="mb-7">
+                <h2 className="font-bold text-2xl sm:text-3xl">Четверти</h2>
+                <p className="text-sm sm:text-base text-muted-foreground">Итоговые оценки по четвертям</p>
+            </div>
+        </section>
+    );
 }
