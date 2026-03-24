f29a0986dd725c9e50d43136eb1908561e793d2d
Chtoś Niechtaŭski <mikhailalexv12@gmail.com>
2026-03-24 15:32:48 +0200

    ESG-19: Create statistics blocks

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,10 +1,12 @@
 import { Hero } from "@/widgets/main-page/ui/Hero/Hero";
+import { StatsGroup } from "@/widgets/main-page/ui/StatsGroup/StatsGroup";
 import { EmptyTodaysSchedule } from "@/widgets/main-page/ui/TodaysSchedule/EmptyTodaysSchedule";
 
 export function MainPage() {
     return (
         <section>
             <Hero />
+            <StatsGroup />
             <EmptyTodaysSchedule />
         </section>
     );
---
+++ src/widgets/main-page/ui/StatsGroup/StatsGroup.tsx
@@ -0,0 +1,33 @@
+import { StatsCard } from "@/shared/ui/StatsCard";
+import { CalendarDays, TrendingUp } from "lucide-react";
+
+export function StatsGroup() {
+    return (
+        <div className="flex mb-5 mt-5 grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4">
+            <StatsCard
+                title="Сегодня уроков"
+                icon={CalendarDays}
+                number={0}
+                subtext="Выходной день"
+            />
+            <StatsCard
+                title="Оценки"
+                icon={TrendingUp}
+                number={0}
+                subtext="за последнее время"
+            />
+            <StatsCard
+                title="Средний балл"
+                icon={TrendingUp}
+                number={0}
+                subtext="за все время"
+            />
+            <StatsCard
+                title="Текущая неделя"
+                icon={CalendarDays}
+                number={0}
+                subtext="учебного года"
+            />
+        </div>
+    );
+}
