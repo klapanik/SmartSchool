c594aac88e33598114a21a26a62650407ad352d8
Chtoś Niechtaŭski <mikhailalexv12@gmail.com>
2026-03-24 16:02:04 +0200

    style: change tab width from 2 to 4

--- src/shared/ui/StatsCard.tsx
+++ src/shared/ui/StatsCard.tsx
@@ -1,22 +1,22 @@
 import type { LucideIcon } from "lucide-react";
 
 type Props = {
-  title: string;
-  icon: LucideIcon;
-  number: number;
-  subtext: string;
+    title: string;
+    icon: LucideIcon;
+    number: number;
+    subtext: string;
 };
 
 export function StatsCard(props: Props) {
-  const { title, number, subtext } = props;
-  return (
-    <div className="primary-block">
-      <div className="flex justify-between">
-        <h3 className="text-sm font-semibold">{title}</h3>
-        <props.icon size={16} />
-      </div>
-      <p className="text-2xl font-bold">{number}</p>
-      <p className="text-xs text-gray-500">{subtext}</p>
-    </div>
-  );
+    const { title, number, subtext } = props;
+    return (
+        <div className="primary-block">
+            <div className="flex justify-between">
+                <h3 className="text-sm font-semibold">{title}</h3>
+                <props.icon size={16} />
+            </div>
+            <p className="text-2xl font-bold">{number}</p>
+            <p className="text-xs text-gray-500">{subtext}</p>
+        </div>
+    );
 }
