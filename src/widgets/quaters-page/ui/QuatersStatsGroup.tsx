d193932e380448cb8f5755eb584d32183c5310dc
Chtoś Niechtaŭski <mikhailalexv12@gmail.com>
2026-03-26 09:51:09 +0200

    feat: make quarters stats group main layout

--- src/widgets/quaters-page/ui/QuatersStatsGroup.tsx
+++ src/widgets/quaters-page/ui/QuatersStatsGroup.tsx
@@ -1,3 +1,27 @@
+import { StatsCard } from "@/shared/ui/StatsCard";
+import { CalendarDays } from "lucide-react";
+
 export const QuatersStatsGroup = () => {
-    return <div>QuatersStatsGroup</div>;
+    return (
+        <div className="flex mb-5 mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3 lg:grid-cols-3">
+            <StatsCard
+                title="Четвертей"
+                icon={CalendarDays}
+                number={4}
+                subtext="добавлено"
+            />
+            <StatsCard
+                title="Четвертных оценок"
+                icon={CalendarDays}
+                number={32}
+                subtext="выставлено"
+            />
+            <StatsCard
+                title="Предметов"
+                icon={CalendarDays}
+                number={16}
+                subtext="изучается"
+            />
+        </div>
+    );
 };
