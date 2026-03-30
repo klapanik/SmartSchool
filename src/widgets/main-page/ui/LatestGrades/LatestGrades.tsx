da8644747b7fc5a94c4f667e42772db19578aff3
Raturchik <artyrkafarov@gmail.com>
2026-03-30 21:48:03 +0300

    fix: fix LatestGrades styles

--- src/widgets/main-page/ui/LatestGrades/LatestGrades.tsx
+++ src/widgets/main-page/ui/LatestGrades/LatestGrades.tsx
@@ -2,19 +2,17 @@ import { TrendingUp } from "lucide-react";
 
 export function LatestGrades() {
     return (
-        <div className="primary-block">
-            <div className="flex flex-col items-center p-6 md:p-12">
-                <div className="mb-6 self-start">
-                    <h3 className="font-semibold text-lg mb-2">Последние оценки</h3>
-                    <p className="text-[#6B7280]">Ваша успеваемость</p>
+        <div className="flex flex-col items-center primary-block">
+            <div className="mb-6 self-start">
+                <h3 className="font-semibold text-lg">Последние оценки</h3>
+                <p className="text-muted-foreground text-sm">Ваша успеваемость</p>
+            </div>
+            <div className="flex w-full flex-col items-center text-sm text-muted-foreground">
+                <div className="mb-4 mx-auto">
+                    <TrendingUp size={48} />
                 </div>
-                <div className="flex w-full max-w-sm min-w-0 flex-col items-center text-sm text-balance gap-0 text-muted-foreground">
-                    <div className="mb-4 mx-auto">
-                        <TrendingUp size={48} />
-                    </div>
-                    <p className="mb-0.5">Пока нет оценок</p>
-                    <p>Начните добавлять свои оценки</p>
-                </div>
+                <p className="mb-0.5">Пока нет оценок</p>
+                <p>Начните добавлять свои оценки</p>
             </div>
         </div>
     );
