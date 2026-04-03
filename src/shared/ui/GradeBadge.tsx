9f3ccf1397b2d198b4c3fc9fa96bd7faed6f8d36
Lizonka-spec <proudoffyou01@gmail.com>
2026-04-03 16:39:20 +0300

    refactor: rename and change type of grade prop

--- src/shared/ui/GradeBadge.tsx
+++ src/shared/ui/GradeBadge.tsx
@@ -1,10 +1,10 @@
 type Props = {
-    mark: number;
+    grade: number | string;
 };
-export function GradeBadge({ mark }: Props) {
+export function GradeBadge({ grade }: Props) {
     return (
         <div className="w-7 bg-primary rounded-full flex justify-center items-center">
-            <span className="text-white text-sm font-semibold">{mark}</span>
+            <span className="text-white text-sm font-semibold">{grade}</span>
         </div>
     );
 }
--- src/shared/ui/SubjectGrade.tsx
+++ src/shared/ui/SubjectGrade.tsx
@@ -2,14 +2,14 @@ import { GradeBadge } from "./GradeBadge";
 
 type SubjectProps = {
     subject: string;
-    mark: number;
+    grade: number | string;
 };
 
-export const SubjectGrade = ({ subject, mark }: SubjectProps) => {
+export const SubjectGrade = ({ subject, grade }: SubjectProps) => {
     return (
         <div className="flex items-center justify-between w-full p-4 bg-white border border-gray-200 rounded-xl">
             <span className="font-semibold">{subject}</span>
-            <GradeBadge mark={mark} />
+            <GradeBadge grade={grade} />
         </div>
     );
 };
