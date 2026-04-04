77d8701818c910de8ae272a33fe88200efd01bf1
Konstantin <lapanik.kostik@gmail.com>
2026-04-04 19:32:47 +0300

    ESG-31: create schedule page with title and subtitle only
    * ESG-31: create schedule page with title and subtitle only
    
    * feat: add title and subtitle for schedule page

---
+++ src/pages/SchedulePage/SchedulePage.tsx
@@ -0,0 +1,10 @@
+export function SchedulePage() {
+    return (
+        <section>
+            <div className="mb-7">
+                <h2 className="page-title">Расписание</h2>
+                <p className="page-subtitle">Управление расписанием уроков</p>
+            </div>
+        </section>
+    );
+}
