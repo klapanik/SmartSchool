817a24000b04094bec5b27d4bc62e4bcecf6a5d4
Konstantin <lapanik.kostik@gmail.com>
2026-04-05 19:32:38 +0300

    ESG-36: create schedule page with title and subtitle only
    * ESG-36: create schedule page with title and subtitle only
    
    * feat: create basic analytics page layout with title and subtitle

--- src/widgets/AppSidebar/model/links.ts
+++ src/widgets/AppSidebar/model/links.ts
@@ -1,9 +1,10 @@
 import type { NavLinksType } from "./types";
 
-import { BookOpen, CalendarDays, House } from "lucide-react";
+import { BookOpen, CalendarDays, ChartNoAxesCombined, House } from "lucide-react";
 
 export const links: NavLinksType[] = [
     { icon: House, title: "Главная", url: "/home" },
     { icon: BookOpen, title: "Расписание", url: "/schedule" },
     { icon: CalendarDays, title: "Четверти", url: "/quarters" },
+    { icon: ChartNoAxesCombined, title: "Аналитика", url: "/analytics" },
 ];
