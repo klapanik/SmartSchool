4dd8f170b5bc3582673f187a853814217613eb13
Kostik Lapanik <lapanik.kostik@gmail.com>
2026-04-05 18:41:25 +0300

    feat: create basic analytics page layout with title and subtitle

--- src/pages/AnalyticsPage/AnalyticsPage.tsx
+++ src/pages/AnalyticsPage/AnalyticsPage.tsx
@@ -1,3 +1,10 @@
 export function AnalyticsPage() {
-    return <div>AnalyticsPage</div>;
+    return (
+        <section>
+            <div className="mb-7">
+                <h2 className="page-title">Аналитика</h2>
+                <p className="page-subtitle">Подробная аналитика вашей успеваемости</p>
+            </div>
+        </section>
+    );
 }
