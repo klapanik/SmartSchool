31c92b8a8e4893e4e15c0f2fe4ccbbb1cb87d770
VladimirYastrebov <vladimir.i.yastrebov@gmail.com>
2026-04-11 20:08:03 +0300

    style: set white background, improve hover and selection states

--- src/features/DatePicker/DatePicker.tsx
+++ src/features/DatePicker/DatePicker.tsx
@@ -5,53 +5,44 @@ import { type DateRange } from "react-day-picker";
 
 import { Button } from "@/components/ui/button";
 import { Calendar } from "@/components/ui/calendar";
-import { Field } from "@/components/ui/field";
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 
 export function DatePicker() {
-    const [date, setDate] = React.useState<DateRange | undefined>({
+    const [date] = React.useState<DateRange | undefined>({
         from: new Date(new Date().getFullYear(), 0, 20),
         to: addDays(new Date(new Date().getFullYear(), 0, 20), 20),
     });
 
     return (
-        <Field
-            className="w-full max-w-sm mx-auto rounded-lg border bg-card p-4 flex justify-between transition-all duration-500 cursor-pointer
-                shadow-[0_4px_20px_-2px_#7c3aed3f] hover:shadow-[0_4px_20px_-2px_#7c3aed88] flex-col @lg:flex-row"
-        >
-            <Popover>
-                <PopoverTrigger asChild>
-                    <Button
-                        variant="outline"
-                        id="date-picker-range"
-                        className="w-full justify-start px-3 font-normal text-left"
-                    >
-                        <CalendarIcon className="mr-2 h-4 w-4" />
-                        {date?.from ? (
-                            date.to ? (
-                                <>
-                                    {format(date.from, "LLL dd, y")} -{" "}
-                                    {format(date.to, "LLL dd, y")}
-                                </>
-                            ) : (
-                                format(date.from, "LLL dd, y")
-                            )
+        <Popover>
+            <PopoverTrigger asChild>
+                <Button
+                    variant="outline"
+                    id="date-picker-range"
+                    className="w-full justify-center px-3 font-normal bg-white hover:bg-smoky-white max-w-[20%]"
+                >
+                    <CalendarIcon className="mr-2 h-4 w-4" />
+                    {date?.from ? (
+                        date.to ? (
+                            <>
+                                {format(date.from, "LLL dd, y")} - {format(date.to, "LLL dd, y")}
+                            </>
                         ) : (
-                            <span>Pick a date</span>
-                        )}
-                    </Button>
-                </PopoverTrigger>
-                <PopoverContent className="w-auto p-0" align="start">
-                    <Calendar
-                        mode="range"
-                        defaultMonth={date?.from}
-                        selected={date}
-                        onSelect={setDate}
-                        numberOfMonths={2}
-                        className="rounded-md border shadow-lg"
-                    />
-                </PopoverContent>
-            </Popover>
-        </Field>
+                            format(date.from, "LLL dd, y")
+                        )
+                    ) : (
+                        <span>Pick a date</span>
+                    )}
+                </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-auto p-0" align="start">
+                <Calendar
+                    mode="single"
+                    defaultMonth={date?.from}
+                    numberOfMonths={2}
+                    className="rounded-md border shadow-lg data-[selected-single=true]:bg-amber-700"
+                />
+            </PopoverContent>
+        </Popover>
     );
 }
--- src/pages/SchedulePage/SchedulePage.tsx
+++ src/pages/SchedulePage/SchedulePage.tsx
@@ -3,11 +3,13 @@ import { DatePicker } from "@/features/DatePicker/DatePicker";
 export function SchedulePage() {
     return (
         <section>
-            <div className="mb-7">
-                <h2 className="page-title">Расписание</h2>
-                <p className="page-subtitle">Управление расписанием уроков</p>
+            <div className="flex justify-between">
+                <div className="mb-7">
+                    <h2 className="page-title">Расписание</h2>
+                    <p className="page-subtitle">Управление расписанием уроков</p>
+                </div>
+                <DatePicker />
             </div>
-            <DatePicker></DatePicker>
         </section>
     );
 }
