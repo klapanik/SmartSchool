619ae86a73edb9f94db856c0c56c39a05bbadd8f
Lizonka-spec <proudoffyou01@gmail.com>
2026-04-15 23:23:52 +0300

    feat: Implement the logic for rendering spans with the grade status, approximate or missing

--- src/shared/ui/SubjectGrade.tsx
+++ src/shared/ui/SubjectGrade.tsx
@@ -9,7 +9,16 @@ type SubjectProps = {
 export const SubjectGrade = ({ subject, grade, isApproximately }: SubjectProps) => {
     return (
         <div className="flex items-center justify-between px-3 py-4 w-full max-h-15 bg-white border border-gray-200 rounded-xl">
-            <span className="font-semibold md:text-m">{subject}</span>
+            <div className="flex flex-col">
+                <span className="font-semibold md:text-m">{subject}</span>
+                {!grade && grade !== 0 ? (
+                    <span className="text-sm text-gray-400">Нет оценок</span>
+                ) : (
+                    isApproximately && (
+                        <span className="text-sm text-gray-400">Примерная: {grade}</span>
+                    )
+                )}
+            </div>
             <GradeBadge
                 grade={!grade && grade !== 0 ? "-" : isApproximately ? `~${grade}` : grade}
             />
