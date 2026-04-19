a5ebb45ef3115c2becebf3ed6f1eb70b5fae26fa
Chtoś Niechtaŭski <mikhailalexv12@gmail.com>
2026-04-19 16:26:22 +0300

    fix: fix adaptivity

--- src/widgets/analytics-page/ui/AnalyticsStatsGroup.tsx
+++ src/widgets/analytics-page/ui/AnalyticsStatsGroup.tsx
@@ -3,55 +3,59 @@ import { CalendarDays, TriangleAlert, Trophy } from "lucide-react";
 
 export function AnalyticStatsGroup() {
     return (
-        <div className="grid grid-cols-1 gap-5 xl:grid-cols-4 md:grid-cols-2">
-            <AnalyticStatsCard
-                title="Пропуски"
-                titleSubtext="за четверть"
-                number={3}
-                icon={CalendarDays}
-                prevNumber={3}
-            />
-            <AnalyticStatsCard
-                title="Общий средний
+        <section>
+            <div className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4 mb-5">
+                <AnalyticStatsCard
+                    title="Пропуски"
+                    titleSubtext="за четверть"
+                    number={3}
+                    icon={CalendarDays}
+                    prevNumber={3}
+                />
+                <AnalyticStatsCard
+                    title="Общий средний
  балл"
-                titleSubtext="за четверть"
-                number={9.23}
-                icon={CalendarDays}
-                prevNumber={8.93}
-            />
-            <AnalyticStatsCard
-                title="Худшая оценка"
-                titleSubtext="по Химии"
-                number={7}
-                icon={CalendarDays}
-                prevNumber={5}
-                description="по Математике"
-            />
-            <AnalyticStatsCard
-                title="Всего оценок"
-                titleSubtext="за четверть"
-                number={126}
-                icon={CalendarDays}
-                prevNumber={159}
-            />
-            <div className="col-span-1 sm:col-span-2 lg:col-span-2">
+                    titleSubtext="за четверть"
+                    number={9.23}
+                    icon={CalendarDays}
+                    prevNumber={8.93}
+                />
                 <AnalyticStatsCard
-                    title="Лучший предмет"
-                    icon={Trophy}
-                    prevNumber={9.5}
-                    subject="География"
-                    number={10.0}
+                    title="Худшая оценка"
+                    titleSubtext="по Химии"
+                    number={7}
+                    icon={CalendarDays}
+                    prevNumber={5}
+                    description="по Математике"
+                />
+                <AnalyticStatsCard
+                    title="Всего оценок"
+                    titleSubtext="за четверть"
+                    number={126}
+                    icon={CalendarDays}
+                    prevNumber={159}
                 />
             </div>
-            <div className="col-span-1 sm:col-span-2 lg:col-span-2">
-                <AnalyticStatsCard
-                    title="Трудный предмет"
-                    subject="Химия"
-                    number={7.2}
-                    icon={TriangleAlert}
-                    prevNumber={8.1}
-                />
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-5">
+                <div>
+                    <AnalyticStatsCard
+                        title="Лучший предмет"
+                        icon={Trophy}
+                        prevNumber={9.5}
+                        subject="География"
+                        number={10.0}
+                    />
+                </div>
+                <div>
+                    <AnalyticStatsCard
+                        title="Трудный предмет"
+                        subject="Химия"
+                        number={7.2}
+                        icon={TriangleAlert}
+                        prevNumber={8.1}
+                    />
+                </div>
             </div>
-        </div>
+        </section>
     );
 }
