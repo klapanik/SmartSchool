eb64c2821750d8da6ae42fc6a8e51522b739ab22
VladimirYastrebov <vladimir.i.yastrebov@gmail.com>
2026-04-20 19:03:03 +0300

    fix: user now see which date he picked

--- src/features/DatePicker/DatePicker.tsx
+++ src/features/DatePicker/DatePicker.tsx
@@ -1,17 +1,13 @@
 import { useState } from "react";
-import { addDays, format } from "date-fns";
+import { format } from "date-fns";
 import { CalendarIcon } from "lucide-react";
-import type { DateRange } from "react-day-picker";
 
 import { Button } from "@/components/ui/button";
 import { Calendar } from "@/components/ui/calendar";
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 
 export function DatePicker() {
-    const [date, setDate] = useState<DateRange | undefined>({
-        from: new Date(new Date().getFullYear(), 0, 20),
-        to: addDays(new Date(new Date().getFullYear(), 0, 20), 20),
-    });
+    const [date, setDate] = useState<Date | undefined>(new Date(new Date().getFullYear(), 0, 20));
 
     return (
         <Popover>
@@ -21,14 +17,8 @@ export function DatePicker() {
                     className="w-full justify-center px-3 font-normal bg-white hover:bg-smoky-white max-w-[20%] cursor-pointer"
                 >
                     <CalendarIcon className="mr-2 size-4" />
-                    {date?.from ? (
-                        date.to ? (
-                            <>
-                                {format(date.from, "LLL dd, y")} - {format(date.to, "LLL dd, y")}
-                            </>
-                        ) : (
-                            format(date.from, "LLL dd, y")
-                        )
+                    {date ? (
+                        format(date, "LLL dd, y")
                     ) : (
                         <span>Pick a date</span>
                     )}
@@ -37,7 +27,9 @@ export function DatePicker() {
             <PopoverContent className="w-auto p-0" align="start">
                 <Calendar
                     mode="single"
-                    defaultMonth={date?.from}
+                    selected={date}
+                    onSelect={setDate}
+                    defaultMonth={date}
                     numberOfMonths={2}
                     className="rounded-md border shadow-lg"
                 />
