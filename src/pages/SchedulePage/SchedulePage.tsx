bb8a3b9dd6ebd22f188ec9196c1e84b6d872452d
VladimirYastrebov <vladimir.i.yastrebov@gmail.com>
2026-04-20 18:51:18 +0300

    refactor: remove the odd div in SchedulePage

--- src/pages/SchedulePage/SchedulePage.tsx
+++ src/pages/SchedulePage/SchedulePage.tsx
@@ -3,11 +3,9 @@ import { DatePicker } from "@/features/DatePicker/DatePicker";
 export function SchedulePage() {
     return (
         <section>
-            <div className="flex justify-between">
-                <div className="mb-7">
-                    <h2 className="page-title">Расписание</h2>
-                    <p className="page-subtitle">Управление расписанием уроков</p>
-                </div>
+            <div className="flex justify-between mb-7">
+                <h2 className="page-title">Расписание</h2>
+                <p className="page-subtitle">Управление расписанием уроков</p>
                 <DatePicker />
             </div>
         </section>
