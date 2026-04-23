26257b5cef58d3e10bf2119ff77be7e350457c83
Chtoś Niechtaŭski <mikhailalexv12@gmail.com>
2026-04-23 22:44:19 +0300

    fix: fix adaptivity and fonts of analytics stats cards

--- src/shared/ui/AnalyticStatsCard.tsx
+++ src/shared/ui/AnalyticStatsCard.tsx
@@ -22,74 +22,86 @@ export function AnalyticStatsCard(props: Props) {
             : null;
 
     return (
-        <div className="primary-block">
-            <div className="flex justify-between mb-2">
-                <div>
-                    <div className="flex gap-1 mb-1 items-center">
-                        <props.icon size={16} />
-                        <h3 className=" font-semibold">{title}</h3>
+        <div
+            className={`${subject ? "block lg:flex justify-between flex-wrap " : ""} primary-block`}
+        >
+            <div>
+                <div className="flex justify-between mb-2">
+                    <div>
+                        <div className="flex gap-1 mb-1 items-center">
+                            <props.icon size={16} />
+                            <h3 className="text-sm font-semibold">{title}</h3>
+                        </div>
+                        {titleSubtext && (
+                            <p className="text-xs text-gray-500">
+                                {titleSubtext}
+                            </p>
+                        )}
                     </div>
-                    {titleSubtext && (
-                        <p className="text-sm text-gray-500">{titleSubtext}</p>
+                    {number && subject === undefined && (
+                        <p className="text-2xl font-bold">{number}</p>
                     )}
                 </div>
-                {number && subject === undefined && (
-                    <p className="text-2xl font-bold">{number}</p>
+
+                {subject && (
+                    <div>
+                        <p className="text-xl font-bold mb-1">{subject}</p>
+                        <div className="text-gray-500 flex gap-1 mb-1">
+                            <p className="text-xs text-gray-500">
+                                средний балл:
+                            </p>
+                            <p className="text-xs text-gray-500">{number}</p>
+                        </div>
+                    </div>
                 )}
             </div>
 
-            {subject && (
-                <div>
-                    <p className="text-xl font-bold mb-1">{subject}</p>
-                    <div className="text-gray-500 flex gap-1 mb-1">
-                        <p>средний балл:</p>
-                        <p>{number}</p>
-                    </div>
-                </div>
-            )}
-
-            {prevNumber !== undefined && (
-                <div>
-                    <div className="flex gap-1">
-                        <p>В прошлой четверти: </p>
-                        <p>{prevNumber}</p>
-                    </div>
-
-                    {statNumber !== null && (
-                        <div className="flex items-center gap-1">
-                            {statNumber > 0 ? (
-                                <div className="flex items-center gap-1 text-green-500">
-                                    <Triangle
-                                        size={12}
-                                        className="fill-current"
-                                    />
-                                    <p>+{statNumber}</p>
-                                </div>
-                            ) : statNumber < 0 ? (
-                                <div className="flex items-center gap-1 text-red-500">
-                                    <Triangle
-                                        size={12}
-                                        className="rotate-180 fill-current "
-                                        color="red"
-                                    />
-                                    <p>{statNumber}</p>
-                                </div>
-                            ) : (
-                                <div className="flex items-center gap-1">
-                                    <Equal
-                                        size={16}
-                                        className="text-gray-500"
-                                    />
-                                    <p className="text-gray-500">0</p>
-                                </div>
-                            )}
+            <div>
+                {prevNumber !== undefined && (
+                    <div>
+                        <div className="flex gap-1 mb-1">
+                            <p className="text-sm">В прошлой четверти: </p>
+                            <p className="text-sm">{prevNumber}</p>
                         </div>
-                    )}
-                    {description && (
-                        <p className="text-gray-500">{description}</p>
-                    )}
-                </div>
-            )}
+
+                        {statNumber !== null && (
+                            <div className="flex items-center gap-1">
+                                {statNumber > 0 ? (
+                                    <div className="flex items-center gap-1 text-green-500">
+                                        <Triangle
+                                            size={12}
+                                            className="fill-current"
+                                        />
+                                        <p className="text-sm">+{statNumber}</p>
+                                    </div>
+                                ) : statNumber < 0 ? (
+                                    <div className="flex items-center gap-1 text-red-500">
+                                        <Triangle
+                                            size={12}
+                                            className="rotate-180 fill-current "
+                                            color="red"
+                                        />
+                                        <p className="text-sm">{statNumber}</p>
+                                    </div>
+                                ) : (
+                                    <div className="flex items-center gap-1">
+                                        <Equal
+                                            size={16}
+                                            className="text-gray-500"
+                                        />
+                                        <p className="text-gray-500">0</p>
+                                    </div>
+                                )}
+                            </div>
+                        )}
+                        {description && (
+                            <p className="text-xs text-gray-500">
+                                {description}
+                            </p>
+                        )}
+                    </div>
+                )}
+            </div>
         </div>
     );
 }
