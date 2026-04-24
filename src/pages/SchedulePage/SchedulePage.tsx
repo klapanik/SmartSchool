0744eb5f42708697faa6046d6ba546ba41ee2fa4
VladimirYastrebov <vladimir.i.yastrebov@gmail.com>
2026-04-24 21:10:35 +0300

    fix: change adaptive from sm to lg

--- src/pages/SchedulePage/SchedulePage.tsx
+++ src/pages/SchedulePage/SchedulePage.tsx
@@ -3,7 +3,7 @@ import { DatePicker } from "@/features/DatePicker/DatePicker";
 export function SchedulePage() {
     return (
         <section>
-            <div className="mb-7 flex flex-col gap-4 lg:flex-row lg:items-center lg:justify-between">
+            <div className="mb-7 flex flex-col gap-4 sm:flex-row sm:items-center sm:justify-between">
                 <div className="space-y-1">
                     <h2 className="page-title">Расписание</h2>
                     <p className="page-subtitle">Управление расписанием уроков</p>
