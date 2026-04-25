494c3eac70e9ebe143f6eb5410b93d5a44db9fbd
Kostik Lapanik <lapanik.kostik@gmail.com>
2026-04-25 14:41:54 +0300

    feat: add mock data for pie chart

--- src/pages/AnalyticsPage/model/mock.ts
+++ src/pages/AnalyticsPage/model/mock.ts
@@ -10,3 +10,16 @@ export const avarageGradeDynamicsChartData = [
     { month: "Апрель", value: 9.8 },
     { month: "Май", value: 8.7 },
 ];
+
+export const pieChartData = [
+    { grade: 10, procent: 25, fill: "var(--chart-1)" },
+    { grade: 9, procent: 21, fill: "var(--chart-2)" },
+    { grade: 8, procent: 15, fill: "var(--chart-3)" },
+    { grade: 7, procent: 12, fill: "var(--chart-4)" },
+    { grade: 6, procent: 8, fill: "var(--chart-5)" },
+    { grade: 5, procent: 5, fill: "var(--chart-6)" },
+    { grade: 4, procent: 4, fill: "var(--chart-7)" },
+    { grade: 3, procent: 7, fill: "var(--chart-8)" },
+    { grade: 2, procent: 2, fill: "var(--chart-9)" },
+    { grade: 1, procent: 1, fill: "var(--chart-10)" },
+];
