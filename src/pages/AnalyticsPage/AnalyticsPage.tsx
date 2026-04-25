8c3c1f8f202c9c0af414d3ef362dbc658c8bbf4a
Kostik Lapanik <lapanik.kostik@gmail.com>
2026-04-25 19:47:39 +0300

    feat: improve gaps on analytics page

--- src/pages/AnalyticsPage/AnalyticsPage.tsx
+++ src/pages/AnalyticsPage/AnalyticsPage.tsx
@@ -3,8 +3,8 @@ import { AnalyticsCharts } from "@/widgets/analytics-page/ui/AnalyticsCharts";
 
 export function AnalyticsPage() {
     return (
-        <section className="@container">
-            <div className="mb-7">
+        <section className="@container flex flex-col gap-7">
+            <div>
                 <h2 className="page-title">Аналитика</h2>
                 <p className="page-subtitle">Подробная аналитика вашей успеваемости</p>
             </div>
