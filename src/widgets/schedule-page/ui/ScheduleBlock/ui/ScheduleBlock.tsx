b3301ec8a069df00ef46c7bf8c10ef6c16758286
Lizonka-spec <proudoffyou01@gmail.com>
2026-04-28 21:07:53 +0300

    fix: fix the drawing of the schedule day header

--- src/widgets/schedule-page/ui/ScheduleBlock/ui/ScheduleBlock.tsx
+++ src/widgets/schedule-page/ui/ScheduleBlock/ui/ScheduleBlock.tsx
@@ -12,26 +12,24 @@ export function ScheduleBlock({ schedule, subjects }: Props) {
     return (
         <div>
             <div>
-                <div>
-                    {dayNumber === 0 ? (
-                        <div>
-                            <Calendar />
-                            <h2>Расписание на сегодня</h2>
-                            <span>{data}</span>
-                        </div>
-                    ) : dayNumber === 1 ? (
-                        <h2>"Понедельник"</h2>
-                    ) : dayNumber === 2 ? (
-                        <h2>"Вторник"</h2>
-                    ) : dayNumber === 3 ? (
-                        <h2>"Среда"</h2>
-                    ) : dayNumber === 4 ? (
-                        <h2>"Четверг"</h2>
-                    ) : (
-                        <h2>"Пятница"</h2>
-                    )}
-                </div>
-                <span>{dayNumber === 0 ? null : subjects.length}</span>
+                {dayNumber === 0 ? (
+                    <div>
+                        <Calendar />
+                        <h2>Расписание на сегодня</h2>
+                        <span>{data}</span>
+                    </div>
+                ) : dayNumber === 1 ? (
+                    <h2>"Понедельник"</h2>
+                ) : dayNumber === 2 ? (
+                    <h2>"Вторник"</h2>
+                ) : dayNumber === 3 ? (
+                    <h2>"Среда"</h2>
+                ) : dayNumber === 4 ? (
+                    <h2>"Четверг"</h2>
+                ) : (
+                    <h2>"Пятница"</h2>
+                )}
+                {dayNumber === 0 ? null : <span>{subjects.length} уроков</span>}
             </div>
             <div>
                 {subjects.map((s, i) => (
