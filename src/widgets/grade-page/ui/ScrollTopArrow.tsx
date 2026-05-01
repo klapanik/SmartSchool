135b3d07778378a0684e5f44faf43b148e7008ac
Kostik Lapanik <lapanik.kostik@gmail.com>
2026-05-01 11:07:57 +0300

    feat: add scroll to top arrow component

--- src/widgets/grade-page/ui/ScrollTopArrow.tsx
+++ src/widgets/grade-page/ui/ScrollTopArrow.tsx
@@ -1,3 +1,36 @@
+import { Button } from "@/components/ui/button";
+import { MoveUp } from "lucide-react";
+import { useEffect, useState } from "react";
+
 export function ScrollTopArrow() {
-    return <div>ScrollTopArrow</div>;
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY >= 600) {
+                setIsVisible(true);
+            } else {
+                setIsVisible(false);
+            }
+        };
+
+        window.addEventListener("scroll", handleScroll);
+    }, []);
+
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        });
+    };
+
+    return (
+        <Button
+            className={`${isVisible ? "fixed" : "hidden"} 
+                bottom-8 right-10 rounded-full size-12 text-white cursor-pointer`}
+            onClick={scrollToTop}
+        >
+            <MoveUp className="size-5" />
+        </Button>
+    );
 }
