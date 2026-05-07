74e1f1cdaed13604e7200ee4bc6fa55bcf7c8e2c
VladimirYastrebov <vladimir.i.yastrebov@gmail.com>
2026-05-07 19:10:36 +0300

    feat: add 2 simple selects to analitics Page

--- src/pages/AnalyticsPage/AnalyticsPage.tsx
+++ src/pages/AnalyticsPage/AnalyticsPage.tsx
@@ -1,5 +1,6 @@
 import { AnalyticStatsGroup } from "@/widgets/analytics-page/ui/AnalyticsStatsGroup";
 import { AnalyticsCharts } from "@/widgets/analytics-page/ui/AnalyticsCharts";
+import { AnaliticsFilters } from "@/widgets/analytics-page/ui/AnaliticsFilters";
 
 export function AnalyticsPage() {
     return (
@@ -8,7 +9,8 @@ export function AnalyticsPage() {
                 <h2 className="page-title">Аналитика</h2>
                 <p className="page-subtitle">Подробная аналитика вашей успеваемости</p>
             </div>
-
+            
+            <AnaliticsFilters></AnaliticsFilters>
             <AnalyticStatsGroup />
             <AnalyticsCharts />
         </section>
---
+++ src/widgets/analytics-page/ui/AnaliticsFilters.tsx
@@ -0,0 +1,31 @@
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select";
+
+export function AnaliticsFilters() {
+    return (
+        <div>
+            <Select>
+                <SelectTrigger className="w-full max-w-48">
+                    <SelectValue placeholder="Select a fruit" />
+                </SelectTrigger>
+                <SelectContent>
+                    <SelectGroup>
+                        <SelectLabel>Fruits</SelectLabel>
+                        <SelectItem value="apple">Apple</SelectItem>
+                        <SelectItem value="banana">Banana</SelectItem>
+                        <SelectItem value="blueberry">Blueberry</SelectItem>
+                        <SelectItem value="grapes">Grapes</SelectItem>
+                        <SelectItem value="pineapple">Pineapple</SelectItem>
+                    </SelectGroup>
+                </SelectContent>
+            </Select>
+        </div>
+    );
+}
