88488d6020c6a6ca73240fa5e3917b600595e080
VladimirYastrebov <vladimir.i.yastrebov@gmail.com>
2026-05-09 10:54:49 +0300

    style: change the value names

--- src/widgets/analytics-page/ui/AnaliticsFilters.tsx
+++ src/widgets/analytics-page/ui/AnaliticsFilters.tsx
@@ -3,7 +3,6 @@ import {
     SelectContent,
     SelectGroup,
     SelectItem,
-    SelectLabel,
     SelectTrigger,
     SelectValue,
 } from "@/components/ui/select";
@@ -11,18 +10,31 @@ import {
 export function AnaliticsFilters() {
     return (
         <div>
-            <Select>
+            <Select defaultValue="all subjects">
                 <SelectTrigger className="w-full max-w-48">
                     <SelectValue placeholder="Select a fruit" />
                 </SelectTrigger>
                 <SelectContent>
                     <SelectGroup>
-                        <SelectLabel>Fruits</SelectLabel>
-                        <SelectItem value="apple">Apple</SelectItem>
-                        <SelectItem value="banana">Banana</SelectItem>
-                        <SelectItem value="blueberry">Blueberry</SelectItem>
-                        <SelectItem value="grapes">Grapes</SelectItem>
-                        <SelectItem value="pineapple">Pineapple</SelectItem>
+                        <SelectItem value="all subjects">Все предметы</SelectItem>
+                        <SelectItem value="bel-lang">Белорусский язык</SelectItem>
+                        <SelectItem value="bel-lit">Белорусская литература</SelectItem>
+                        <SelectItem value="rus-lang">Русский язык</SelectItem>
+                        <SelectItem value="rus-lit">Русская литература</SelectItem>
+                    </SelectGroup>
+                </SelectContent>
+            </Select>
+            <Select defaultValue="current estimates">
+                <SelectTrigger className="w-full max-w-48">
+                    <SelectValue placeholder="Select a fruit" />
+                </SelectTrigger>
+                <SelectContent>
+                    <SelectGroup>
+                        <SelectItem value="current estimates">Текущие оценки</SelectItem>
+                        <SelectItem value="fourth term">4 четверть (2025-2026)</SelectItem>
+                        <SelectItem value="third term">3 четверть (2025-2026)</SelectItem>
+                        <SelectItem value="second term">2 четверть (2025-2026)</SelectItem>
+                        <SelectItem value="first term">1 четверть (2025-2026)</SelectItem>
                     </SelectGroup>
                 </SelectContent>
             </Select>
