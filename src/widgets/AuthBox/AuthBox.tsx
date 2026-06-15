635bde781ca9ae82a5a4a6781206dabf9f71778c
Lizonka-spec <proudoffyou01@gmail.com>
2026-06-16 00:38:43 +0300

    feat: implement and style tabs

--- src/widgets/AuthBox/AuthBox.tsx
+++ src/widgets/AuthBox/AuthBox.tsx
@@ -1,14 +1,34 @@
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { BookOpen } from "lucide-react";
 
 export function AuthBox() {
     return (
-        <div className="bg-smoky-white p-6 flex flex-col rounded-lg">
+        <div className="bg-smoky-white p-6 flex flex-col rounded-lg gap-5">
             <div className="flex flex-col items-center">
                 <div className="flex gap-2 items-center justify-center">
                     <BookOpen />
                     <p className="text-2xl font-bold">Добро пожаловать</p>
                 </div>
                 <span className="text-gray-500">Войдите или создайте новый аккаунт</span>
+            </div>
+
+            <div className="w-full">
+                <Tabs defaultValue="login">
+                    <TabsList className="w-full flex justify-between">
+                        <TabsTrigger
+                            value="login"
+                            className="text-black w-full data-[state=active]:bg-white cursor-pointer"
+                        >
+                            Войти
+                        </TabsTrigger>
+                        <TabsTrigger
+                            value="register"
+                            className="text-black w-full data-[state=active]:bg-white cursor-pointer"
+                        >
+                            Регистрация
+                        </TabsTrigger>
+                    </TabsList>
+                </Tabs>
             </div>
         </div>
     );
