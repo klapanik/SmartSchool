fe28ca0237202408697874af887315e6fe1a7b98
klapanik <lapanikkonstantin@gmail.com>
2026-06-17 18:35:44 +0300

    feat: improve EmptyCurrentQuarter component

--- src/widgets/quarters-page/ui/EmptyCurrentQuarter.tsx
+++ src/widgets/quarters-page/ui/EmptyCurrentQuarter.tsx
@@ -1,26 +1,40 @@
-import { Empty, EmptyContent, EmptyHeader, EmptyTitle, EmptyMedia } from "@/components/ui/empty";
+import { Button } from "@/components/ui/button";
+import {
+    Empty,
+    EmptyContent,
+    EmptyHeader,
+    EmptyTitle,
+    EmptyMedia,
+    EmptyDescription,
+} from "@/components/ui/empty";
 import { CalendarDays } from "lucide-react";
 
 export function EmptyCurrentQuarter() {
     return (
-        <Empty className="md:p-0 p-0">
-            <EmptyHeader className="gap-1 flex-row">
-                <EmptyMedia className="my-auto">
-                    <CalendarDays className="size-7 sm:size-6" />
-                </EmptyMedia>
+        <Empty className="md:p-0 p-0 gap-3">
+            <EmptyHeader className="max-w-none w-full flex-row justify-between">
+                <div className="flex gap-1">
+                    <EmptyMedia className="my-auto">
+                        <CalendarDays className="size-5" />
+                    </EmptyMedia>
 
-                <EmptyTitle className="text-2xl">Каникулы!</EmptyTitle>
+                    <EmptyTitle>Сейчас каникулы!</EmptyTitle>
+                </div>
+
+                <EmptyDescription className="text-muted-foreground">
+                    Четверть не активна
+                </EmptyDescription>
             </EmptyHeader>
 
-            <EmptyContent className="flex-row max-w-lg">
-                <div>
-                    <h4 className="text-base mb-0.5">
-                        Учебных дней пока что нет, отдыхайте!
-                    </h4>
-                    <p className="text-sm text-muted-foreground">
-                        Но на каникулах помните про правила безопасности!
-                    </p>
-                </div>
+            <EmptyContent className="max-w-none pl-5 text-muted-foreground text-start text-wrap items-start gap-0">
+                <p>
+                    В настоящее время учебный процесс приостановлен. Информация о новой четверти
+                    появится после начала занятий. В свободное время рекомендуем ознакомиться с
+                    заданной на лето литературой:
+                </p>
+                <Button variant="link" className="px-0 underline cursor-pointer">
+                    Литература на лето
+                </Button>
             </EmptyContent>
         </Empty>
     );
