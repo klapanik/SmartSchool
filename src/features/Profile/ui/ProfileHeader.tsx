f3faf937f14bc128ae71ae4f9e1d3e53f1af3e9f
klapanik <lapanikkonstantin@gmail.com>
2026-06-18 16:56:06 +0300

    feat: add right part of profile header

--- src/features/Profile/ui/ProfileHeader.tsx
+++ src/features/Profile/ui/ProfileHeader.tsx
@@ -1,3 +1,17 @@
+import { GraduationCap } from "lucide-react";
+
 export function ProfileHeader() {
-    return <div>ProfileHeader</div>;
+    return (
+        <div className="bg-primary rounded-t-md p-5 text-white">
+            <div></div>
+            <div>
+                <h3 className="mb-2 font-semibold">Константин Лапаник</h3>
+                <div className="w-min font-semibold rounded-xl flex gap-1 text-xs bg-accent px-2.5 py-1">
+                    <GraduationCap size={15} className="my-auto mr-0.5" />
+                    <span>9А</span>
+                    <span>класс</span>
+                </div>
+            </div>
+        </div>
+    );
 }
