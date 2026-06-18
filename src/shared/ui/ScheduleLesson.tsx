b25f564f922aa420ac29acfee0aff1fd2b2af261
Lizonka-spec <proudoffyou01@gmail.com>
2026-06-18 23:15:34 +0300

    fix: Add a render condition for the now label.

--- src/shared/ui/ScheduleLesson.tsx
+++ src/shared/ui/ScheduleLesson.tsx
@@ -28,9 +28,11 @@ export function ScheduleLesson({ subject, number, time, grade, isCurrentLesson }
                     <div className="flex items-center">
                         <Clock4 size={13} className="text-gray-500 mr-1" />
                         <span className="text-gray-500 text-sm mr-2">{time}</span>
-                        <div className="bg-primary rounded-full px-2.5 py-0.5 text-white text-xs">
-                            <span>Сейчас</span>
-                        </div>
+                        {isCurrentLesson && (
+                            <div className="bg-primary rounded-full px-2.5 py-0.5 text-white text-xs">
+                                <span>Сейчас</span>
+                            </div>
+                        )}
                     </div>
                 </div>
             </div>
