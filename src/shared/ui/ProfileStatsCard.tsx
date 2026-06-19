9d0b2e9bfabc8bedd305ba0c3e48d77cab8494a4
Chtoś Niechtaŭski <mikhailalexv12@gmail.com>
2026-06-19 11:43:22 +0300

    feat: add basic profile stats card component

--- src/shared/ui/ProfileStatsCard.tsx
+++ src/shared/ui/ProfileStatsCard.tsx
@@ -1,3 +1,18 @@
-export const ProfileStatsCard = () => {
-  return <div></div>;
+import type { LucideIcon } from "lucide-react";
+
+type Props = {
+  icon: LucideIcon;
+  number: number;
+  title: string;
+};
+
+export const ProfileStatsCard = (props: Props) => {
+  const { number, title } = props;
+  return (
+    <div className="flex items-center justify-center">
+      <props.icon size={11} />
+      <p className="text-xl font-bold">{number}</p>
+      <p className="text-xs text-gray-500">{title}</p>
+    </div>
+  );
 };
