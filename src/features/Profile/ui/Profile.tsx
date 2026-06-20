29ffa9461ddb068cda290ed11e921efa45a971e1
Chtoś Niechtaŭski <mikhailalexv12@gmail.com>
2026-06-20 22:09:35 +0300

    ESG-62: Add statisctics block to profile (#39)
    * ESG-62: Add statistics blocks to user profile
    
    * feat: add basic profile stats card component
    
    * feat: add basic profile stats widget
    
    * feat: improve ui and add stats block in profile
    
    * feat: add container
    
    * fix: fix numbers size
    
    * fix: work fixes
    
    * fix: after review fixes

--- src/features/Profile/ui/Profile.tsx
+++ src/features/Profile/ui/Profile.tsx
@@ -1,9 +1,13 @@
 import { ProfileHeader } from "./ProfileHeader";
+import { ProfileStatsCards } from "./ProfileStatsCards";
 
 export function Profile() {
     return (
         <section className="rounded-md">
             <ProfileHeader />
+            <div className="px-5">
+                <ProfileStatsCards />
+            </div>
         </section>
     );
 }
---
+++ src/features/Profile/ui/ProfileStatsCards.tsx
@@ -0,0 +1,12 @@
+import { ProfileStatsCard } from "@/shared/ui/ProfileStatsCard";
+import { BookOpen, TrendingUp, Trophy } from "lucide-react";
+
+export function ProfileStatsCards() {
+    return (
+        <div className="grid grid-cols-3 gap-2.5 mt-5">
+            <ProfileStatsCard icon={TrendingUp} number={9.47} title="средний" />
+            <ProfileStatsCard icon={Trophy} number={76} title="оценок" />
+            <ProfileStatsCard icon={BookOpen} number={13} title="предметов" />
+        </div>
+    );
+}
