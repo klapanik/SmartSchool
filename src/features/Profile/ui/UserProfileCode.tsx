527dd2c5fa30a4a90d4af242a6a198cc2d538584
Raturchik <artyrkafarov@gmail.com>
2026-06-21 22:50:21 +0300

    feat: create userProfileCode layout

--- src/features/Profile/ui/UserProfileCode.tsx
+++ src/features/Profile/ui/UserProfileCode.tsx
@@ -1,3 +1,15 @@
+import { Copy } from "lucide-react";
+
 export function UserProfileCode() {
-    return <div>UserProfileCode</div>;
+    return (
+        <div className="bg-white rounded-xl flex p-3 justify-between items-center">
+            <div className="">
+                <p className="text-[#6B7280] font-medium">КОД ПОЛЬЗОВАТЕЛЯ</p>
+
+                <p className="text-black font-medium">6338С53...DB7D</p>
+            </div>
+            <Copy color="black" size={18} cursor="pointer" />
+        </div>
+    );
 }
+//1
