15b7d4ba850bcb03a5ca2de3f9fbed258e4307c8
Raturchik <artyrkafarov@gmail.com>
2026-06-22 22:31:56 +0300

    feat: create copy-to-clipboard function

--- src/features/Profile/ui/UserProfileCode.tsx
+++ src/features/Profile/ui/UserProfileCode.tsx
@@ -1,14 +1,32 @@
 import { Copy } from "lucide-react";
+import { useState } from "react";
 
 export function UserProfileCode() {
+    const [input, setInput] = useState("6338С53...DB7D");
+
+    async function copyToClipboard() {
+        try {
+            await navigator.clipboard.writeText(input);
+            console.log(input);
+        } catch (err) {
+            console.error(err);
+        }
+    }
     return (
         <div className="bg-white rounded-xl flex p-3 justify-between items-center">
             <div className="">
-                <p className="text-[#6B7280] font-medium">КОД ПОЛЬЗОВАТЕЛЯ</p>
+                <p className="text-[#6B7280] text-[10px] font-medium">КОД ПОЛЬЗОВАТЕЛЯ</p>
 
-                <p className="text-black font-medium">6338С53...DB7D</p>
+                <input
+                    type="text"
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}
+                    className="text-black text-[10px] font-medium bg-transparent outline-none w-full"
+                />
             </div>
-            <Copy color="black" size={18} cursor="pointer" />
+            <button onClick={copyToClipboard}>
+                <Copy color="black" size={18} cursor="pointer" />
+            </button>
         </div>
     );
 }
