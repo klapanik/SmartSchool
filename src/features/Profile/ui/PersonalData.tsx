0430092f16503e4db15518bd7616528779e5f142
Lizonka-spec <proudoffyou01@gmail.com>
2026-06-23 19:32:55 +0300

    feat: Implement editable fields

--- src/features/Profile/ui/PersonalData.tsx
+++ src/features/Profile/ui/PersonalData.tsx
@@ -7,8 +7,13 @@ export function PersonalData() {
             <p className="text-sm text-smoky-black">Личные данные</p>
             <PersonalDataItem Icon={GraduationCap} title="Кл. руководитель" value="Не указан" />
             <PersonalDataItem Icon={Users} title="Родитель" value="Не указан" />
-            <PersonalDataItem Icon={Phone} title="Телефон" value="Не указан" />
-            <PersonalDataItem Icon={User} title="Email" value="lapanikkonstantin@gmail.com" />
+            <PersonalDataItem Icon={Phone} title="Телефон" value="Не указан" isMutable />
+            <PersonalDataItem
+                Icon={User}
+                title="Email"
+                value="lapanikkonstantin@gmail.com"
+                isMutable
+            />
         </div>
     );
 }
--- src/shared/ui/PersonalDataItem.tsx
+++ src/shared/ui/PersonalDataItem.tsx
@@ -1,19 +1,27 @@
-import type { LucideIcon } from "lucide-react";
+import { Pencil, type LucideIcon } from "lucide-react";
 
 type Props = {
     Icon: LucideIcon;
     title: string;
     value: string;
+    isMutable?: boolean;
 };
 
-export const PersonalDataItem = ({ Icon, title, value }: Props) => {
+export const PersonalDataItem = ({ Icon, title, value, isMutable }: Props) => {
     return (
         <div className="flex justify-between items-center gap-4">
             <div className="flex gap-1">
                 <Icon className="text-gray-500 size-3.5 m-auto" />
                 <span className="text-gray-500 text-sm">{title}</span>
             </div>
-            <span className="text-sm text-smoky-black">{value}</span>
+            <div className="flex items-center">
+                <span className="text-sm text-smoky-black mr-1">{value}</span>
+                {isMutable && (
+                    <button>
+                        <Pencil className="size-3 text-primary" />
+                    </button>
+                )}
+            </div>
         </div>
     );
 };
