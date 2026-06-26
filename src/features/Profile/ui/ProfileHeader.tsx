d99dfa4f308997cabe86fbff597b79898b8c125c
Артур <artyrkafarov@gmail.com>
2026-06-26 19:13:11 +0300

    Feature-ESG-61:Add user code block with copy functionality (#41)

--- src/features/Profile/ui/ProfileHeader.tsx
+++ src/features/Profile/ui/ProfileHeader.tsx
@@ -1,27 +1,32 @@
 import { GraduationCap, PlusIcon, UserRound } from "lucide-react";
 import { Avatar, AvatarFallback, AvatarImage, AvatarBadge } from "@/components/ui/avatar";
+import { UserProfileCode } from "./UserProfileCode";
 
 export function ProfileHeader() {
     return (
-        <div className="flex gap-3 bg-primary rounded-t-md p-5 text-white">
-            <Avatar className="border-3 border-[#B897F1]" size="lg">
-                <AvatarImage src="" alt="SS" />
-                <AvatarFallback className="bg-accent text-white">
-                    <UserRound />
-                </AvatarFallback>
-                <AvatarBadge className="bg-white text-primary duration-150 hover:scale-110 hover:rotate-90">
-                    <PlusIcon />
-                </AvatarBadge>
-            </Avatar>
+        <div className="flex-col bg-linear-to-r from-[#8141ED] to-[#9257ED] rounded-t-md p-5 text-white ">
+            <div className="flex gap-3 mb-2.5">
+                <Avatar className="border-3 border-[#B897F1]" size="lg">
+                    <AvatarImage src="" alt="SS" />
+                    <AvatarFallback className="bg-accent text-white">
+                        <UserRound />
+                    </AvatarFallback>
+                    <AvatarBadge className="bg-white text-primary duration-150 hover:scale-110 hover:rotate-90">
+                        <PlusIcon />
+                    </AvatarBadge>
+                </Avatar>
 
-            <div className="my-auto">
-                <h3 className="mb-1.5 font-semibold">Константин Лапаник</h3>
-                <div className="w-min font-semibold rounded-xl flex gap-1 text-xs bg-accent px-2.5 py-1 cursor-pointer">
-                    <GraduationCap size={15} className="my-auto mr-0.5" />
-                    <span>9А</span>
-                    <span>класс</span>
+                <div className="my-auto">
+                    <h3 className="mb-1.5 font-semibold">Константин Лапаник</h3>
+                    <div className="w-min font-semibold rounded-xl flex gap-1 text-xs bg-accent px-2.5 py-1 cursor-pointer">
+                        <GraduationCap size={15} className="my-auto mr-0.5" />
+                        <span>9А</span>
+                        <span>класс</span>
+                    </div>
                 </div>
             </div>
+
+            <UserProfileCode />
         </div>
     );
 }
