a6ba7885edc478bcf7ffc7cd25291aab897c6fff
Chtoś Niechtaŭski <mikhailalexv12@gmail.com>
2026-07-17 19:03:25 +0300

    feat: Add basic styles

--- frontend/src/features/Settings/Settings.tsx
+++ frontend/src/features/Settings/Settings.tsx
@@ -1,3 +1,10 @@
 export const Settings = () => {
-    return <div>Settings</div>;
+    return (
+        <div>
+            <p className="text-sm text-smoky-black mb-3.5">Настройки</p>
+            <button className="w-full h-7 bg-[#EF5353] text-white rounded-lg font-bold cursor-pointer text-xs active:bg-[#EF6B6B] hover:shadow-md active:shadow-inner">
+                Удалить аккаунт
+            </button>
+        </div>
+    );
 };
