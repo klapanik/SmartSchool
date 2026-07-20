f4f3e8db166e2b77bfe1bc9d769d84d87440d753
klapanik <lapanikkonstantin@gmail.com>
2026-07-20 19:31:14 +0300

    feat: improve auth layout, make good adaptive, delete unnecessary code

--- frontend/src/layouts/AuthLayout.tsx
+++ frontend/src/layouts/AuthLayout.tsx
@@ -6,7 +6,7 @@ export function AuthLayout() {
     const currentUrl = location.pathname;
 
     return (
-        <section className="h-screen w-full flex flex-col justify-center items-center bg-[url('/auth_background.png')] bg-cover bg-no-repeat bg-center">
+        <section className="h-screen w-full px-3 flex flex-col justify-center items-center bg-[url('/auth_background.png')] bg-cover bg-no-repeat bg-center">
             <div className="flex flex-col gap-5 items-center mb-7.5">
                 <GraduationCap
                     strokeWidth={1.5}
@@ -30,9 +30,7 @@ export function AuthLayout() {
                                 : ""}
                     </span>
                 </div>
-                <div className="w-full">
-                    <Outlet />
-                </div>
+                <Outlet />
             </div>
         </section>
     );
