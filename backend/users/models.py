2c7518ea7ad37be1af0c43fa8d22d3f01607dfd7
klapanik <lapanikkonstantin@gmail.com>
2026-07-25 20:53:20 +0300

    ESG-71: create basic models in backend

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class UserRole(models.TextChoices):
+    STUDENT = "student", "Student"
+    TEACHER = "teacher", "Teacher"
+    PARENT = "parent", "Parent"
+    ADMIN = "admin", "Administrator"
+
+
+class User(AbstractUser):
+    # id, is_active, fisrt_name, last_name from AbstractUser
+    email = models.EmailField(
+        unique=True,
+        verbose_name="Email"
+    )
