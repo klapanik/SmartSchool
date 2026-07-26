948bd9b8d20f76dd7411fa8f8764bc10649e3b6b
klapanik <lapanikkonstantin@gmail.com>
2026-07-26 14:06:08 +0300

    feat: create all models for smart_school app

--- backend/smart_school/admin.py
+++ backend/smart_school/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(School)
+admin.site.register(SchoolClass)
+
+admin.site.register(Grade)
+admin.site.register(QuarterGrade)
+
+admin.site.register(Quarter)
+admin.site.register(Subject)
+admin.site.register(ScheduleLesson)
--- backend/smart_school/models.py
+++ backend/smart_school/models.py
@@ -1,3 +1,141 @@
 from django.db import models
 
-# Create your models here.
+
+class School(models.Model):
+    name = models.CharField(max_length=255)
+    city = models.CharField(max_length=100)
+    address = models.CharField(max_length=255)
+
+    director = models.ForeignKey(
+        "users.Teacher",
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class SchoolClass(models.Model):
+    school = models.ForeignKey(
+        "School",
+        on_delete=models.CASCADE,
+        related_name="classes",
+    )
+
+    form = models.PositiveSmallIntegerField()
+    letter = models.CharField(max_length=2)
+
+    class_teacher = models.ForeignKey(
+        "users.Teacher",
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="managed_classes",
+    )
+
+    def __str__(self):
+        return f"{self.form}{self.letter}"
+
+
+class Quarter(models.Model):
+    school = models.ForeignKey(
+        "School",
+        on_delete=models.CASCADE,
+        related_name="quarters",
+    )
+
+    number = models.PositiveSmallIntegerField()
+    starts_at = models.DateField()
+    ends_at = models.DateField()
+
+    class Meta:
+        unique_together = (
+            "school",
+            "number",
+        )
+
+
+class Subject(models.Model):
+    name = models.CharField(
+        max_length=100,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Grade(models.Model):
+    student = models.ForeignKey(
+        "users.Student",
+        on_delete=models.CASCADE,
+        related_name="grades",
+    )
+
+    subject = models.ForeignKey(
+        "Subject",
+        on_delete=models.CASCADE,
+        related_name="grades"
+    )
+
+    teacher = models.ForeignKey(
+        "users.Teacher",
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+
+    grade = models.DecimalField(
+        max_digits=3,
+        decimal_places=1,
+    )
+
+    date = models.DateField()
+
+    comment = models.TextField(
+        blank=True,
+    )
+
+
+class QuarterGrade(models.Model):
+    student = models.ForeignKey(
+        "users.Student",
+        on_delete=models.CASCADE,
+        related_name="quarter_grades",
+    )
+
+    subject = models.ForeignKey(
+        "Subject",
+        on_delete=models.CASCADE,
+        related_name="quarter_grades"
+    )
+
+    quarter = models.ForeignKey(
+        "Quarter",
+        on_delete=models.CASCADE,
+    )
+
+    grade = models.DecimalField(
+        max_digits=3,
+        decimal_places=1,
+    )
+
+
+class ScheduleLesson(models.Model):
+    school_class = models.ForeignKey(
+        "SchoolClass",
+        on_delete=models.CASCADE,
+        related_name="lessons"
+    )
+
+    subject = models.ForeignKey(
+        "Subject",
+        on_delete=models.CASCADE,
+    )
+
+    weekday = models.PositiveSmallIntegerField()
+    lesson_number = models.PositiveSmallIntegerField()
+    starts_at = models.TimeField()
+    ends_at = models.TimeField()
+
+    classroom = models.CharField(
+        max_length=20,
+    )
