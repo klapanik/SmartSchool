a654c9677b0b436f8060d275e2a63c356e586c57
klapanik <lapanikkonstantin@gmail.com>
2026-07-28 21:33:44 +0300

    feat: create pathes for getting tokens and refreshing them

--- backend/core/api/urls.py
+++ backend/core/api/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('user/', include("users.api.urls")),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
