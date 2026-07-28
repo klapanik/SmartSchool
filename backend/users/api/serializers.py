0369b66509c773aefb5ee25d364f981b7d4fa0d2
klapanik <lapanikkonstantin@gmail.com>
2026-07-28 21:31:51 +0300

    feat: remake UserActivation serializer

--- backend/users/api/serializers.py
+++ backend/users/api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
-from ..models import User, UserActivation
 
 
 class UserActivationSerializer(serializers.Serializer):
-    code = serializers.CharField(max_length=20)
+    code = serializers.CharField(max_length=15, required=True)
+    email = serializers.EmailField(required=True)
+    password = serializers.CharField(required=True)
