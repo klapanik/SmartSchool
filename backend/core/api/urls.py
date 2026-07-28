6edc6a9454adbbfd62bf0430c1754eae39abcfd5
klapanik <lapanikkonstantin@gmail.com>
2026-07-28 21:33:01 +0300

    feat: delete url router

--- backend/core/api/urls.py
+++ backend/core/api/urls.py
@@ -1,11 +1,5 @@
-from rest_framework.routers import DefaultRouter
-from users.api.urls import users_router
 from django.urls import path, include
 
-router = DefaultRouter()
-
-router.registry.extend(users_router.registry)
-
 urlpatterns = [
-    path('', include(router.urls))
+    path('user/', include("users.api.urls")),
 ]
