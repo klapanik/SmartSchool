fae8a7f63261f9c40f40859db3c9357197b95a83
Lizonka-spec <proudoffyou01@gmail.com>
2026-07-28 19:22:24 +0300

    fix: delete unnecessory exports

--- frontend/src/features/Settings/index.ts
+++ frontend/src/features/Settings/index.ts
@@ -1,4 +1,1 @@
-export { AccentColor } from "./ui/AccentColor";
-export { TimeFormat } from "./ui/TimeFormat";
-export { WeekBegin } from "./ui/WeekBegin";
 export { Settings } from "./ui/Settings";
