f753e9ba8919586013b484f116a66c2b9fb57046
klapanik <lapanikkonstantin@gmail.com>
2026-07-29 10:40:53 +0300

    feat: create logout api view, serializer for it and url path

--- backend/users/api/serializers.py
+++ backend/users/api/serializers.py
@@ -5,3 +5,7 @@ class UserActivationSerializer(serializers.Serializer):
     code = serializers.CharField(max_length=15, required=True)
     email = serializers.EmailField(required=True)
     password = serializers.CharField(required=True)
+
+
+class LogoutSerializer(serializers.Serializer):
+    refresh = serializers.CharField(required=True)
--- backend/users/api/views.py
+++ backend/users/api/views.py
@@ -5,11 +5,14 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.exceptions import TokenError
 
 import bcrypt
 
 from ..models import UserActivation
-from .serializers import UserActivationSerializer
+from .serializers import UserActivationSerializer, LogoutSerializer
 
 
 class UserActivationView(APIView):
@@ -50,3 +53,20 @@ class UserActivationView(APIView):
         refresh_token = str(refresh)
 
         return Response({"access": access_token, "refresh": refresh_token}, status=status.HTTP_200_OK)
+
+
+class UserLogoutAPIView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        serializer = LogoutSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        try:
+            refresh = RefreshToken(serializer.validated_data["refresh"])
+            refresh.blacklist()
+        except TokenError:
+            return Response({"detail": "Invalid refresh token."}, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
