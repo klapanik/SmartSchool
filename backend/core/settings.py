e18f2fc87143875a74b1a2842e832caef47f7c98
klapanik <lapanikkonstantin@gmail.com>
2026-07-30 16:25:28 +0300

    fix: fix mistake with CORS policy

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -59,7 +59,7 @@ REST_FRAMEWORK = {'DEFAULT_PERMISSION_CLASSES': [
     'rest_framework.permissions.AllowAny'
 ]}
 
-CORS_OROGIN_ALLOW_ALL = True
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'core.urls'
 
