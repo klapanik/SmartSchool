8b396c9ca781390fc40735797cccd6b8d0e26f14
VladimirYastrebov <vladimir.i.yastrebov@gmail.com>
2026-08-03 14:46:13 +0300

    ESG-77: Create url, views for editing user's data. Add email check

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager as DjangoUserManager
 from django.db import models
 from django.conf import settings
 
@@ -8,6 +8,31 @@ class UserRole(models.TextChoices):
     TEACHER = "teacher", "Teacher"
     PARENT = "parent", "Parent"
     ADMIN = "admin", "Administrator"
+
+
+class UserManager(DjangoUserManager):
+    use_in_migrations = False
+
+    def create_user(self, email=None, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(email, password, **extra_fields)
 
 
 class User(AbstractUser):
@@ -42,6 +67,8 @@ class User(AbstractUser):
         blank=True,
         null=True,
     )
+
+    objects = UserManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
