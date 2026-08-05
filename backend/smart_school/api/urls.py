34318c8dd44b819c9ba831469c11ed7132d105b6
klapanik <lapanikkonstantin@gmail.com>
2026-08-05 12:17:46 +0300

    feat: create url path for quarters view

--- backend/smart_school/api/urls.py
+++ backend/smart_school/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ScheduleView, GradesView, GradeAverageView
+from .views import ScheduleView, GradesView, GradeAverageView, QuartersView
 
 
 urlpatterns = [
     path('schedule/', ScheduleView.as_view(), name='schedule'),
     path('grades/', GradesView.as_view(), name='grades'),
     path('grades/average/', GradeAverageView.as_view(), name='average'),
+    path('quarters/', QuartersView.as_view(), name='quarters'),
 ]
