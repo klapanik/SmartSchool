89917aa21684291ecaf0e43d0d67334a467a5796
klapanik <lapanikkonstantin@gmail.com>
2026-08-05 11:55:59 +0300

    feat: connecct url and grades view

--- backend/smart_school/api/urls.py
+++ backend/smart_school/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ScheduleView
+from .views import ScheduleView, GradesView
 
 
 urlpatterns = [
-    path('schedule/', ScheduleView.as_view(), name='schedule')
+    path('schedule/', ScheduleView.as_view(), name='schedule'),
+    path('grades/', GradesView.as_view(), name='grades'),
 ]
