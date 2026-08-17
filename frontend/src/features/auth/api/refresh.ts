e5cfeafa05b89ef6bf7dd5dd6439410f68f41ce6
klapanik <lapanikkonstantin@gmail.com>
2026-08-17 14:08:34 +0300

    feat: create refresh function, integrate it into apiFetch function

---
+++ frontend/src/features/auth/api/refresh.ts
@@ -0,0 +1,18 @@
+import { BASE_URL } from "@/shared/api/config";
+import { setAccessToken } from "../model/token-storage";
+
+export async function refreshAccessToken() {
+    const response = await fetch(`${BASE_URL}/user/refresh/`, {
+        method: "POST",
+        credentials: "include",
+    });
+
+    if (!response.ok) {
+        throw new Error("Failed to refresh token");
+    }
+
+    const data = await response.json();
+
+    setAccessToken(data.access);
+    return data.access;
+}
--- frontend/src/shared/api/fetch.ts
+++ frontend/src/shared/api/fetch.ts
@@ -1,16 +1,32 @@
+import { refreshAccessToken } from "@/features/auth/api/refresh";
 import { BASE_URL } from "./config";
 import { getAccessToken } from "@/features/auth/model/token-storage";
 
 export async function apiFetch<T>(endpoint: string, options: RequestInit = {}): Promise<T> {
     const token = getAccessToken();
 
-    const response = await fetch(`${BASE_URL}${endpoint}`, {
+    let response = await fetch(`${BASE_URL}${endpoint}`, {
         ...options,
         headers: {
             "Content-Type": "application/json",
             ...(token ? { Authorization: `Bearer ${token}` } : {}),
         },
+        credentials: "include",
     });
+
+    if (response.status === 401) {
+        console.log(response.status === 401);
+        const newAccessToken = await refreshAccessToken();
+
+        response = await fetch(`${BASE_URL}${endpoint}`, {
+            ...options,
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json",
+                ...(token ? { Authorization: `Bearer ${newAccessToken}` } : {}),
+            },
+        });
+    }
 
     if (!response.ok) {
         const errorData = await response.json();
