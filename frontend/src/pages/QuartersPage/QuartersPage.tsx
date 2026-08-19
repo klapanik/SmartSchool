ec49ef495ec7980fcb0c804371e78d6023dd4d82
klapanik <lapanikkonstantin@gmail.com>
2026-08-19 17:55:38 +0300

    feat: delete mock data, make quarters data fetching, improve ui

--- frontend/src/pages/QuartersPage/QuartersPage.tsx
+++ frontend/src/pages/QuartersPage/QuartersPage.tsx
@@ -1,18 +1,32 @@
+import { useQuartersQuery } from "@/entities/quarter/api/query";
+import type { Quarter } from "@/entities/quarter/model/type";
+
 import { CurrentQuarter } from "@/widgets/quarters-page/ui/CurrentQuarter";
 import { EmptyCurrentQuarter } from "@/widgets/quarters-page/ui/EmptyCurrentQuarter";
 import { QuartersBlock } from "@/widgets/quarters-page/ui/QuartersBlock";
-import {
-    q1,
-    q2,
-    q3,
-    q4,
-    subjects,
-    year,
-} from "@/widgets/quarters-page/ui/QuartersBlock/model/mock";
 import { QuartersStatsGroup } from "@/widgets/quarters-page/ui/QuartersStatsGroup";
 
+import { Skeleton } from "@/components/ui/skeleton";
+import { useQuartersGradesQuery } from "@/entities/grades/api/queries";
+
 export function QuartersPage() {
-    const currentQuarter: { isOver: boolean } = { isOver: true };
+    const {
+        data: quartersData,
+        isLoading: isQuartersLoading,
+        isError: isQuartersError,
+        error: quartersError,
+    } = useQuartersQuery();
+
+    const {
+        data: quartersGradesData,
+        isLoading: isQuartersGradesLoading,
+        isError: isQuartersGradesError,
+        error: quartersGradesError,
+    } = useQuartersGradesQuery();
+
+    const currentQuarter: Quarter | null = quartersData
+        ? quartersData.filter((quarter) => quarter.is_current)[0]
+        : null;
 
     return (
         <section className="@container">
@@ -21,18 +35,70 @@ export function QuartersPage() {
                 <p className="page-subtitle">Итоговые оценки по четвертям</p>
             </div>
 
-            <div className="primary-block">
-                {currentQuarter.isOver ? <EmptyCurrentQuarter /> : <CurrentQuarter />}
-            </div>
-            <QuartersStatsGroup />
+            {isQuartersError || isQuartersGradesError ? (
+                <div className="primary-block flex gap-1 text-lg">
+                    <span>Ошибка в расписании!</span>
+                    <span>
+                        <i>{String(quartersError ?? '') + " " + String(quartersGradesError ?? '')}</i>
+                    </span>
+                </div>
+            ) : isQuartersLoading ||
+              isQuartersGradesLoading ||
+              !quartersData ||
+              !quartersGradesData ? (
+                <div className="flex flex-col gap-5">
+                    <Skeleton className="w-full h-44" />
 
-            <div className="flex flex-col gap-4">
-                <QuartersBlock quarter={q1} subjects={subjects} />
-                <QuartersBlock quarter={q2} subjects={subjects} />
-                <QuartersBlock quarter={q3} subjects={subjects} />
-                <QuartersBlock quarter={q4} subjects={subjects} />
-                <QuartersBlock quarter={year} subjects={subjects} />
-            </div>
+                    <div className="flex gap-5">
+                        <Skeleton className="w-full h-30" />
+                        <Skeleton className="w-full h-30" />
+                        <Skeleton className="w-full h-30" />
+                    </div>
+
+                    <Skeleton className="w-full h-51" />
+                    <Skeleton className="w-full h-51" />
+                    <Skeleton className="w-full h-51" />
+                    <Skeleton className="w-full h-51" />
+                </div>
+            ) : (
+                <>
+                    <div className="primary-block">
+                        {currentQuarter ? (
+                            <CurrentQuarter
+                                number={currentQuarter.number}
+                                starts_at={currentQuarter.starts_at}
+                                ends_at={currentQuarter.ends_at}
+                                average={
+                                    quartersGradesData.filter(
+                                        (q) => q.quarter_id === currentQuarter.id,
+                                    )[0].average_grade
+                                }
+                            />
+                        ) : (
+                            <EmptyCurrentQuarter />
+                        )}
+                    </div>
+                    <QuartersStatsGroup />
+
+                    <div className="flex flex-col gap-4">
+                        {quartersData.map((quarter) => {
+                            const quarterGrade = quartersGradesData.filter((q) => q.quarter_id === quarter.id)[0]
+
+                            return (
+                                <QuartersBlock
+                                    key={quarter.id}
+                                    number={quarter.number}
+                                    average={quarterGrade.average_grade}
+                                    starts_at={quarter.starts_at}
+                                    ends_at={quarter.ends_at}
+                                    quarterGrades={quarterGrade.quarter_grades}
+                                />
+                            );
+                        })}
+                        {/* <QuartersBlock quarter={year} subjects={subjects} /> */}
+                    </div>
+                </>
+            )}
         </section>
     );
 }
---
+++ frontend/src/widgets/quarters-page/ui/QuartersBlock.tsx
@@ -0,0 +1,64 @@
+import { SubjectGrade } from "@/shared/ui/SubjectGrade";
+import type { QuarterGrade } from "@/entities/grades/model/types";
+
+type Props = {
+    number: string | number;
+    average: string | number;
+    starts_at: string;
+    ends_at: string;
+    quarterGrades: QuarterGrade[];
+};
+
+export const QuartersBlock = ({ number, average, starts_at, ends_at, quarterGrades }: Props) => {
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+
+    const [sYear, sMonth, sDay] = starts_at.split("-");
+    const startDate = new Date(Number(sYear), Number(sMonth) - 1, Number(sDay));
+
+    const [eYear, eMonth, eDay] = ends_at.split("-");
+    const endDate = new Date(Number(eYear), Number(eMonth) - 1, Number(eDay));
+
+    const isQuarterRunning = today >= startDate && today <= endDate;
+
+    const formattedStartDate = startDate.toLocaleDateString("ru-RU", {
+        day: "numeric",
+        month: "long",
+        year: "numeric",
+    });
+
+    const formattedEndDate = endDate.toLocaleDateString("ru-RU", {
+        day: "numeric",
+        month: "long",
+        year: "numeric",
+    });
+
+    return (
+        <div className="flex flex-col primary-block gap-6">
+            <div className="flex flex-row justify-between">
+                <div className="flex flex-col">
+                    <h2 className="text-lg font-bold text-gray-800">{number} четверть</h2>
+                    <span className="text-sm text-gray-500 mt-1">
+                        {formattedStartDate} - {formattedEndDate}
+                    </span>
+                </div>
+                <div className="flex flex-col text-right">
+                    <p className="text-lg font-bold ">
+                        {isQuarterRunning ? `~${average}` : average}
+                    </p>
+                    <span className="text-gray-500">Средний балл</span>
+                </div>
+            </div>
+            <div className="grid grid-cols-1 gap-2 @min-[720px]:grid-cols-2 @min-[800px]:grid-cols-3">
+                {quarterGrades.map((grade: QuarterGrade) => (
+                    <SubjectGrade
+                        key={grade.id}
+                        subject={grade.subject}
+                        grade={grade.grade}
+                        isApproximately={false}
+                    />
+                ))}
+            </div>
+        </div>
+    );
+};
