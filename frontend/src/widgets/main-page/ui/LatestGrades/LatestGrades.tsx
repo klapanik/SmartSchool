2b1eca81ab512baf2c4862d833ae0a08a66abb5d
klapanik <lapanikkonstantin@gmail.com>
2026-08-21 09:40:51 +0300

    feat: improve LatestGrades component

--- frontend/src/widgets/main-page/ui/LatestGrades/LatestGrades.tsx
+++ frontend/src/widgets/main-page/ui/LatestGrades/LatestGrades.tsx
@@ -7,11 +7,11 @@ export function LatestGrades() {
                 <h3 className="font-semibold text-lg">Последние оценки</h3>
                 <p className="text-muted-foreground text-sm">Ваша успеваемость</p>
             </div>
-            <div className="flex w-full flex-col items-center text-sm text-muted-foreground">
+            <div className="flex w-full flex-col items-center text-muted-foreground my-auto">
                 <div className="mb-4 mx-auto">
                     <TrendingUp size={48} />
                 </div>
-                <p className="mb-0.5">Пока нет оценок</p>
+                <p>Пока нет оценок</p>
                 <p>Начните добавлять свои оценки</p>
             </div>
         </div>
